67d627256548d53ec49353ef75b3f4fb27f80055
aibhleog <aibhleog@tamu.edu>
2022-08-15 22:20:32 -0400

    adding skeleton website structure :bone:

---
+++ includes/footer.js
@@ -0,0 +1,136 @@
+// Footer 
+//document.write("<div id='footer'>");
+document.write("<div class='container'>");
+//document.write("<div class='row'>");
+
+// Tweets 
+//document.write("<section class='col-4 col-12-mobile'>");
+//document.write("<header>");
+//document.write("<h2 class='icon brands fa-twitter circled'><span class='label'>Tweets</span></h2>");
+//document.write("</header>");
+//document.write("<ul class='divided'>");
+//document.write("<li>");
+//document.write("<article class='tweet'>");
+//document.write("Amet nullam fringilla nibh nulla convallis tique ante sociis accumsan.");
+//document.write("<span class='timestamp'>5 minutes ago</span>");
+//document.write("</article>");
+//document.write("</li>");
+//document.write("<li>");
+//document.write("<article class='tweet'>");
+//document.write("Hendrerit rutrum quisque.");
+//document.write("<span class='timestamp'>30 minutes ago</span>");
+//document.write("</article>");
+//document.write("</li>");
+//document.write("<li>");
+//document.write("<article class='tweet'>");
+//document.write("Curabitur donec nulla massa laoreet nibh. Lorem praesent montes.");
+//document.write("<span class='timestamp'>3 hours ago</span>");
+//document.write("</article>");
+//document.write("</li>");
+//document.write("<li>");
+//document.write("<article class='tweet'>");
+//document.write("Lacus natoque cras rhoncus curae dignissim ultricies. Convallis orci aliquet.");
+//document.write("<span class='timestamp'>5 hours ago</span>");
+//document.write("</article>");
+//document.write("</li>");
+//document.write("</ul>");
+//document.write("</section>");
+
+// Posts 
+//document.write("<section class='col-4 col-12-mobile'>");
+//document.write("<header>");
+//document.write("<h2 class='icon solid fa-file circled'><span class='label'>Posts</span></h2>");
+//document.write("</header>");
+//document.write("<ul class='divided'>");
+//document.write("<li>");
+//document.write("<article class='post stub'>");
+//document.write("<header>");
+//document.write("<h3><a href='#'>Nisl fermentum integer</a></h3>");
+//document.write("</header>");
+//document.write("<span class='timestamp'>3 hours ago</span>");
+//document.write("</article>");
+//document.write("</li>");
+//document.write("<li>");
+//document.write("<article class='post stub'>");
+//document.write("<header>");
+//document.write("<h3><a href='#'>Phasellus portitor lorem</a></h3>");
+//document.write("</header>");
+//document.write("<span class='timestamp'>6 hours ago</span>");
+//document.write("</article>");
+//document.write("</li>");
+//document.write("<li>");
+//document.write("<article class='post stub'>");
+//document.write("<header>");
+//document.write("<h3><a href='#'>Magna tempus consequat</a></h3>");
+//document.write("</header>");
+//document.write("<span class='timestamp'>Yesterday</span>");
+//document.write("</article>");
+//document.write("</li>");
+//document.write("<li>");
+//document.write("<article class='post stub'>");
+//document.write("<header>");
+//document.write("<h3><a href='#'>Feugiat lorem ipsum</a></h3>");
+//document.write("</header>");
+//document.write("<span class='timestamp'>2 days ago</span>");
+//document.write("</article>");
+//document.write("</li>");
+//document.write("</ul>");
+//document.write("</section>");
+
+// Photos 
+//document.write("<section class='col-4 col-12-mobile'>");
+//document.write("<header>");
+//document.write("<h2 class='icon solid fa-camera circled'><span class='label'>Photos</span></h2>");
+//document.write("</header>");
+//document.write("<div class='row gtr-25'>");
+//document.write("<div class='col-6'>");
+//document.write("<a href='#' class='image fit'><img src='images/pic10.jpg' alt='' /></a>");
+//document.write("</div>");
+//document.write("<div class='col-6'>");
+//document.write("<a href='#' class='image fit'><img src='images/pic11.jpg' alt='' /></a>");
+//document.write("</div>");
+//document.write("<div class='col-6'>");
+//document.write("<a href='#' class='image fit'><img src='images/pic12.jpg' alt='' /></a>");
+//document.write("</div>");
+//document.write("<div class='col-6'>");
+//document.write("<a href='#' class='image fit'><img src='images/pic13.jpg' alt='' /></a>");
+//document.write("</div>");
+//document.write("<div class='col-6'>");
+//document.write("<a href='#' class='image fit'><img src='images/pic14.jpg' alt='' /></a>");
+//document.write("</div>");
+//document.write("<div class='col-6'>");
+//document.write("<a href='#' class='image fit'><img src='images/pic15.jpg' alt='' /></a>");
+///document.write("</div>");
+//document.write("</div>");
+//document.write("</section>");
+
+//document.write("</div>");
+//document.write("<hr />");
+document.write("<div class='row'>");
+document.write("<div class='col-12'>");
+
+// Contact 
+document.write("<section class='contact'>");
+document.write("<header>");
+document.write("<h3>Want to see more?</h3>");
+document.write("</header>");
+document.write("<p>You can find us on our social medias and GitHub!</p>");
+document.write("<ul class='icons'>");
+document.write("<li><a href='https://twitter.com/CosmicSprngJWST' class='icon brands fa-twitter'><span class='label'>Twitter</span></a></li>");
+document.write("<li><a href='https://www.instagram.com/CosmicSpringJWST/' class='icon brands fa-instagram'><span class='label'>Instagram</span></a></li>");
+document.write("<li><a href='https://github.com/cosmic-spring' class='icon brands fa-github'><span class='label'>Pinterest</span></a></li>");
+document.write("</ul>");
+document.write("</section>");
+
+// Copyright 
+document.write("<div class='copyright'>");
+document.write("<ul class='menu'>");
+document.write("<li>&copy; Cosmic Spring. All rights reserved.</li><li>Design: <a href='http://html5up.net'>HTML5 UP</a></li>");
+document.write("</ul>");
+document.write("</div>");
+
+document.write("</div>");
+
+document.write("</div>");
+document.write("</div>");
+//document.write("</div>");
